022fcfa0a984d14ef56614b554b735497a24e270
naabvb <46524999+naabvb@users.noreply.github.com>
2019-12-02 16:49:29 +0200

    Add full iphone x and up support

--- client/src/_components/logout.js
+++ client/src/_components/logout.js
@@ -5,7 +5,10 @@ import axios from 'axios';
 class LogOut extends Component {
     async componentDidMount() {
         await axios.get('/api/clear-role')
-        window.location.reload(true);
+        var useragent = window.navigator.userAgent
+        if (useragent.indexOf("Edge") === -1) { // If not MS Edge
+            window.location.reload(true);
+        }
         window.location.pathname = "/login"
     }
 
