7bf7ff578ee843919c67661ad4b2a535b7714224
naabvb <46524999+naabvb@users.noreply.github.com>
2019-12-05 21:49:47 +0200

    Fixes broken sw from last commit, now unregistering sw when logging out and registering a new one

--- client/src/_components/logout.js
+++ client/src/_components/logout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 import axios from 'axios';
+import * as serviceWorker from '../serviceWorker';
 
 class LogOut extends Component {
     async componentDidMount() {
@@ -9,6 +10,7 @@ class LogOut extends Component {
         if (useragent.indexOf("Edge") === -1) { // If not MS Edge
             window.location.reload(true);
         }
+        serviceWorker.unregister();
         window.location.pathname = "/login"
     }
 
--- client/src/index.js
+++ client/src/index.js
@@ -12,4 +12,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register({immediate: true});
+//serviceWorker.register({immediate: true});
+serviceWorker.register();
