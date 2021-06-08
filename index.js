fd4613a0e6bc8c19c68a157d52ed98f8993a5240
naabvb <46524999+naabvb@users.noreply.github.com>
2021-06-08 18:29:46 +0300

    Add dotenv

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const cookieParser = require('cookie-parser');
-const shrinkRay = require('shrink-ray-current');
-const express = require('express');
-const path = require('path');
-const sslRedirect = require('heroku-ssl-redirect');
+const cookieParser = require("cookie-parser");
+require("dotenv").config({ path: process.env.ENV_FILE_PATH });
+const shrinkRay = require("shrink-ray-current");
+const express = require("express");
+const path = require("path");
+const sslRedirect = require("heroku-ssl-redirect");
 const app = express();
 app.use(shrinkRay());
 
-const { getAlbum } = require('./_services/google-photos'); // Currently not in use
-const { getImages, deleteImage } = require('./_services/aws-photos');
-const { getAuthenticate, getRole } = require('./_services/auth-handler');
+const { getAlbum } = require("./_services/google-photos"); // Currently not in use
+const { getImages, deleteImage } = require("./_services/aws-photos");
+const { getAuthenticate, getRole } = require("./_services/auth-handler");
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(sslRedirect());
 app.use(cookieParser(process.env.signedKey));
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
-app.get('/api/images/:imagesId', async function (request, response) {
+app.get("/api/images/:imagesId", async function (request, response) {
   try {
     if (request.signedCookies.rkey) {
       const role = await getRole(request.signedCookies.rkey);
@@ -27,13 +28,11 @@ app.get('/api/images/:imagesId', async function (request, response) {
       }
       if (role === 2 && (parameter === 3 || parameter === 4)) {
         const images = await getImages(parameter);
-        response.json(images)
-      }
-      else {
+        response.json(images);
+      } else {
         response.sendStatus(401);
       }
-    }
-    else {
+    } else {
       response.sendStatus(401);
     }
   } catch (e) {
@@ -41,72 +40,66 @@ app.get('/api/images/:imagesId', async function (request, response) {
   }
 });
 
-app.get('/api/authenticate', async function (request, response) {
+app.get("/api/authenticate", async function (request, response) {
   try {
     const result = await getAuthenticate(request);
-    let inProd = false;
-    if (process.env.NODE_ENV === 'production') {
-      inProd = true;
-    }
+    let inProd = process.env.NODE_ENV === "production"
     const options = {
       httpOnly: true,
       signed: true,
       secure: inProd,
     };
     if (result.role == 2) {
-      response.cookie('rkey', result.key, options).send({ role: 'vastila' });
+      response.cookie("rkey", result.key, options).send({ role: "vastila" });
     }
     if (result.role == 1) {
-      response.cookie('rkey', result.key, options).send({ role: 'jatkala' });
-    }
-    else {
+      response.cookie("rkey", result.key, options).send({ role: "jatkala" });
+    } else {
       response.sendStatus(401);
     }
-
   } catch (e) {
     response.status(500);
   }
 });
 
-app.get('/api/get-role', async function (request, response) {
+app.get("/api/get-role", async function (request, response) {
   try {
     if (request.signedCookies.rkey) {
       const result = await getRole(request.signedCookies.rkey);
-      if (result === 1) response.send({ role: 'jatkala' });
-      if (result === 2) response.send({ role: 'vastila' });
-      response.send({ role: 'no' });
+      if (result === 1) response.send({ role: "jatkala" });
+      if (result === 2) response.send({ role: "vastila" });
+      response.send({ role: "no" });
+    } else {
+      response.send({ role: "no" });
     }
-    else {
-      response.send({ role: 'no' });
-    }
-
   } catch (e) {
     response.status(500);
   }
 });
 
-app.get('/api/clear-role', async function (request, response) {
-  response.clearCookie('rkey').sendStatus(200);
+app.get("/api/clear-role", async function (request, response) {
+  response.clearCookie("rkey").sendStatus(200);
 });
 
-app.get('/api/delete-image', async function (request, response) {
+app.get("/api/delete-image", async function (request, response) {
   try {
     if (request.signedCookies.rkey) {
       const role = await getRole(request.signedCookies.rkey);
-      if (role === 2 && request.query.img_url && request.query.img_url.length > 0) {
-        const result = await deleteImage(request.query.img_url)
+      if (
+        role === 2 &&
+        request.query.img_url &&
+        request.query.img_url.length > 0
+      ) {
+        const result = await deleteImage(request.query.img_url);
         if (result === true) {
           response.sendStatus(204);
-        }
-        else {
+        } else {
           response.sendStatus(200);
         }
-      }
-      else {
+      } else {
         response.sendStatus(403);
       }
-    }
-    else {
+    } else {
       response.sendStatus(401);
     }
   } catch (e) {
@@ -116,8 +109,8 @@ app.get('/api/delete-image', async function (request, response) {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
