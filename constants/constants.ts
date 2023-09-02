7a5f2f58ca3e1840e5c4ecd33981c0ff8d18193f
naabvb <46524999+naabvb@users.noreply.github.com>
2023-09-02 16:27:39 +0300

    Add v3 and v4

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,5 +1,5 @@
 export const jatkalaRoutes = ['j1', 'j2', 'j3', 'j4'];
-export const vastilaRoutes = ['v1', 'v2'];
+export const vastilaRoutes = ['v1', 'v2', 'v3', 'v4'];
 
 export const buckets = {
   j1: { name: 'riistakamera-j1', trashBucket: 'riistakamera-trash-j1', url: process.env.j1!! },
@@ -8,6 +8,8 @@ export const buckets = {
   j4: { name: 'riistakamera-j4', trashBucket: 'riistakamera-trash-j4', url: process.env.j4!! },
   v1: { name: 'riistakamera-v1', trashBucket: 'riistakamera-trash-v1', url: process.env.v1!! },
   v2: { name: 'riistakamera-v2', trashBucket: 'riistakamera-trash-v2', url: process.env.v2!! },
+  v3: { name: 'riistakamera-v3', trashBucket: 'riistakamera-trash-v3', url: process.env.v3!! },
+  v4: { name: 'riistakamera-v4', trashBucket: 'riistakamera-trash-v4', url: process.env.v4!! },
 };
 
 export enum Cameras {
@@ -17,6 +19,8 @@ export enum Cameras {
   j4 = 'j4',
   v1 = 'v1',
   v2 = 'v2',
+  v3 = 'v3',
+  v4 = 'v4',
 }
 
 export enum Role {
